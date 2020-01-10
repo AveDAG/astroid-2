a8d0b250f143bf5b485e1ddda2649b448a79335d
AveDAG <58709240+AveDAG@users.noreply.github.com>
2020-01-10 14:03:13 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+function hero () {
+    hero2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . 2 . . . 2 . . . . . . . 
+. . . . . 1 2 1 . . . . . . . . 
+. 2 2 2 . 2 2 2 . 2 2 2 . . . . 
+. 2 5 2 . 2 1 2 . 2 5 2 . . . . 
+. 2 2 2 . . 2 . . 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 . . . . . . 
+. . . . . 2 5 2 . . . . . . . . 
+. . . . . 2 2 2 . . . . . . . . 
+. . . . . . 5 . . . . . . . . . 
+. . . . . 2 2 2 . . . . . . . . 
+. . . . . 2 . 2 . . . . . . . . 
+. . . . . 2 . 2 . . . . . . . . 
+`, SpriteKind.Player)
+    hero2.setPosition(78, 97)
+    controller.moveSprite(hero2, 100, 0)
+}
+function score () {
+    info.changeScoreBy(1)
+}
+function overlap () {
+    game.over(false)
+}
+function projectile () {
+    projectile2 = sprites.createProjectileFromSide(img`
+. . . . . . . . . . . . . . . . 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 1 1 1 7 7 7 7 1 1 1 7 7 . 
+. 7 7 1 1 1 7 7 7 7 1 1 1 7 7 . 
+7 7 1 1 1 1 1 7 7 1 1 1 1 1 7 7 
+7 7 1 1 1 1 1 7 7 1 1 1 1 1 7 7 
+7 7 7 1 1 1 7 7 7 7 1 1 1 7 7 7 
+7 7 7 7 7 7 7 f f 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 f 7 7 7 7 7 7 7 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 f f f f f f f 7 7 7 . 
+. 7 7 7 f 1 2 1 2 1 2 1 f 7 7 . 
+. 7 7 7 f 1 1 2 1 2 1 1 f 7 7 . 
+. 7 7 7 7 f f f f f f f 7 7 7 . 
+`, 0, 0)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    overlap()
+})
+let projectile2: Sprite = null
+let hero2: Sprite = null
+hero()
+game.onUpdateInterval(700, function () {
+    projectile()
+    score()
+})
